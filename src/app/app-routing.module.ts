b8576a015d42d9989f53bb1906b6497effe798a3
amsorry <amsorry@amsorrys-MacBook-Air.local>
2023-10-06 18:42:10 +0700

    init form input

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ListComponent } from './pages/order/list/list.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'order', component: OrderComponent,
+    children: [
+      { path: '', component: ListComponent },
+      // { path: 'status/:orderno', component: StatusComponent },
+      { path: ':orderno', component: ListComponent },
+      // { path: ':orderno/:progress', component: OrderdetailComponent }
+    ]
+  },
   { path: '**', component: PagenotfoundComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ListComponent } from './pages/order/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    OrderComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +32,11 @@ import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.compone
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
